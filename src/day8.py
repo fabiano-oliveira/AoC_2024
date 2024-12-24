8d6f47ae969b3487d311ef30e5e4f7a9a868ecf1
Fabiano Oliveira <foliveiraz@gmail.com>
2024-12-24 08:43:15 -0300

    Day8/Part2 = Partially DONE

--- src/day8.py
+++ src/day8.py
@@ -6,10 +6,19 @@ Point = namedtuple("Point", ["row", "column"])
 
 
 class AntennaMap:
-    def __init__(self, map_limit: Point):
+    def __init__(self, map_limit: Point, discover_aligned_antinodes: bool = False):
         self.__map_limit = map_limit
         self.__antennas: Dict[str, List[Point]] = {}
         self.__antinodes: Dict[str, List[Point]] = {}
+        self.__discover_aligned_antinodes = discover_aligned_antinodes
+    
+    @property
+    def discover_aligned_antinodes(self) -> bool:
+        return self.__discover_aligned_antinodes
+
+    @discover_aligned_antinodes.setter
+    def discover_aligned_antinodes(self, value: bool):
+        self.__discover_aligned_antinodes = value
 
     @property
     def Antennas(self):
@@ -21,6 +30,9 @@ class AntennaMap:
 
     def get_unique_antinodes(self) -> set[Point]:
         return set(point for points in self.__antinodes.values() for point in points)
+    
+    def get_all_antinodes(self) -> List[Point]:
+        return [point for points in self.__antinodes.values() for point in points]  
 
     def add_antenna(self, antenna: str, point: Point):
         if antenna not in self.__antennas:
@@ -37,31 +49,64 @@ class AntennaMap:
         if antenna not in self.__antinodes:
             self.__antinodes[antenna] = set()
 
-        if self.__distance(source, target) < 2:
+        if self.__distance(source, target) < 2 and not self.__discover_aligned_antinodes:
             return
 
-        opposite_point = self.__opposite_point(source, target)
-        if not self.is_point_in_map(opposite_point):
-            return
 
-        self.__antinodes[antenna].add(opposite_point)
+        if self.__discover_aligned_antinodes:
+            self.__antinodes[antenna].update(self.__all_opposite_points(source, target))
+        else:
+            opposite_point = self.__opposite_point(source, target)
+            if not self.is_point_in_map(opposite_point):
+                return
+
+            self.__antinodes[antenna].add(opposite_point)
 
     def __distance(self, source: Point, target: Point):
         return abs(source.row - target.row) + abs(source.column - target.column)    
 
-    def __opposite_point(self, source: Point, target: Point):
-        delta_row = target.row - source.row
-        delta_column = target.column - source.column
+    def __opposite_point(self, source: Point, target: Point, multiplier: int = 1):
+        delta_row = (target.row - source.row) * multiplier
+        delta_column = (target.column - source.column) * multiplier
 
         return Point(source.row - delta_row, source.column - delta_column)
+    
+    def __all_opposite_points(self, source: Point, target: Point) -> List[Point]:
+        multiplier = 1
+        points = []
+
+        while True:
+            antinode_point = self.__opposite_point(source, target, multiplier)
+            if self.is_point_in_map(antinode_point):
+               points.append(antinode_point)
+            else:
+                break
+            multiplier += 1
+
+        return points
 
     def is_point_in_map(self, point: Point) -> bool:
         return 0 <= point.row < self.__map_limit.row and \
             0 <= point.column < self.__map_limit.column
 
+    def __str__(self):
+        content = ""
+        antennas = {point: antenna for antenna, points in self.__antennas.items() for point in points}
+        antinodes = {point: "#" for antenna, points in self.__antinodes.items() for point in points}
+        objects_in_map = {**antennas, **antinodes}
+
+        for row in range(self.__map_limit.row):
+            for column in range(self.__map_limit.column):
+                point = Point(row, column)
+                if point in objects_in_map:
+                    content += objects_in_map[point]
+                else:
+                    content += "."
+            content += "\n"
+        return content
 
 def find_all_antennas(data: List[str]) -> AntennaMap:
-    map = AntennaMap(Point(len(data), len(data[0])))
+    map = AntennaMap(Point(len(data), len(data[0])), discover_aligned_antinodes=True)
 
     for row, line in enumerate(data):
         for column, char in enumerate(line):
@@ -71,9 +116,10 @@ def find_all_antennas(data: List[str]) -> AntennaMap:
     return map
 
 def main():
-    data = read_all_lines("./src/day8-input.txt")
+    data = read_all_lines("./src/day8-input-sample.txt")
     map = find_all_antennas(data)
-    antinodes = len(map.get_unique_antinodes())
+    print(map)
+    antinodes = len(map.get_all_antinodes())
     print(f"There is {antinodes} antinodes")
 
 if __name__ == "__main__":
--- tests/test_day8.py
+++ tests/test_day8.py
@@ -53,6 +53,56 @@ def test_should_not_add_antinode_if_distance_is_below_two(map, point1, point2):
 
     assert len(map.get_unique_antinodes()) == 0
 
+def test_aligned_antinodes(map):
+    # arrange
+    map.discover_aligned_antinodes = True
+    map.add_antenna("T", Point(0,0))
+    map.add_antenna("T", Point(1, 3))
+    map.add_antenna("T", Point(2, 1))
+
+    # act
+    antinodes = map.Antinodes["T"]
+
+    # assert
+    assert len(antinodes) == 6
+    assert Point(0, 5) in antinodes
+    assert Point(2, 6) in antinodes
+    assert Point(3, 9) in antinodes
+    assert Point(4, 2) in antinodes 
+    assert Point(6, 3) in antinodes 
+    assert Point(8, 4) in antinodes
+
+
+def test_count_antinodes_duplicated(map):
+    # arrange
+    map.discover_aligned_antinodes = True
+    map.add_antenna("O", Point(2, 5))
+    map.add_antenna("O", Point(3, 7))
+
+    map.add_antenna("A", Point(5, 6))
+    map.add_antenna("A", Point(9, 9))
+
+    # act
+    antinodes = map.get_all_antinodes()
+
+    # assert
+    assert len(antinodes) == 4
+    assert Point(0, 1) in antinodes
+    assert Point(1, 3) in antinodes 
+    assert Point(4, 9) in antinodes
+    assert Point(1, 3) in antinodes
+
+def test_is_in_map(map):
+    # assert
+
+    assert map.is_point_in_map(Point(0, 0))
+    assert map.is_point_in_map(Point(9, 9))
+    assert not map.is_point_in_map(Point(10, 10))
+    assert not map.is_point_in_map(Point(-1, 5))
+    assert not map.is_point_in_map(Point(-1, -1))
+
+
+
 def test_is_point_in_map(map):
     assert map.is_point_in_map(Point(5, 5))
     assert map.is_point_in_map(Point(0, 0))
